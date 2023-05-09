cbad675c5be2213691a796f0f1f37c0474153a0a
jyefi <75103233+jyefi@users.noreply.github.com>
2023-05-09 22:14:23 +0200

    Create docker_shell.sh
    Shell to get Cassandra basic config

---
+++ docker_shell.sh
@@ -0,0 +1,15 @@
+# Create "cassandra" folder (si aún no existe)
+if [ ! -d "cassandra" ]; then
+  mkdir cassandra
+fi
+
+# Create folders "data" y "scripts" inside "cassandra" folder
+mkdir -p cassandra/data
+mkdir -p cassandra/scripts
+
+# Download "docker-compose.yml" file from GitHub
+curl -o cassandra/docker-compose.yml https://raw.githubusercontent.com/jyefi/cassandra-docker/docker-compose.yml
+
+# Executes docker-compose up
+cd cassandra
+docker-compose up
